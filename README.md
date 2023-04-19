4b211500383e7903e18a49f0606826120ea1413a
tiatiahwang <tiahwang44@gmail.com>
2023-04-18 19:42:25 -0700

    edit README and create NavBar component

--- README.md
+++ README.md
@@ -2,5 +2,21 @@
 
 ### 1. 설치
 
-> `npx create-next-app@latest`<br/>
-> `npx create-next-app@latest --typescript`
+`npx create-next-app@latest`<br/>
+`npx create-next-app@latest --typescript`
+
+### 2. NextJS는 Framework
+
+파일 이름이나 구조 등을 정해진 규칙에 따라 만들어야 함<br/>
+
+- 라이브러리: 사용자가 파일 이름이나 구조 등을 정할 수 있음 (ex.리액트)
+
+### 3. pages 폴더
+
+- 해당 폴더 안에 있는 파일의 이름이 바로 url 주소가 됨
+- `index.js`: 홈 화면
+
+### 4. SSR
+
+- 초기 상태로 pre-rendering(사용자가 빈 화면을 보게 하지 않음)
+- SEO에 좋음
---
+++ components/NavBar.js
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+export default function NavBar() {
+  const router = useRouter();
+  return (
+    <nav>
+      <Link
+        href='/'
+        style={{ color: router.pathname === '/' ? 'red' : 'blue' }}
+      >
+        HOME
+      </Link>
+      <Link
+        href='/about'
+        style={{ color: router.pathname === '/about' ? 'red' : 'blue' }}
+      >
+        ABOUT
+      </Link>
+    </nav>
+  );
+}
